b2db041deb0a5992e4863b220bf39a9ac8632366
Suman Kumar <suman.k@skysoft.net.in>
2019-11-13 14:55:26 +0530

    =34

---
+++ src/components/feed/feed.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Feed extends React.Component{
+    render(){
+        return (
+            <h1>I am feed</h1>
+        );
+    }
+}
+
+export default Feed;
---
+++ src/components/hot-topics/hot-topic.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class HotTopic extends React.Component{
+
+    render(){
+        return(
+            <h1>I am hot topic</h1>
+        );
+    }
+}
+
+export default HotTopic;
