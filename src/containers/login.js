6a6b76e7f3d5985d9a61355982085f91fda1cea5
Neeraj Kumar <neeraja.singh@hotmail.com>
2019-11-26 04:04:18 +0530

    img fix

--- src/containers/login.js
+++ src/containers/login.js
@@ -1,12 +1,11 @@
 import React, { useState } from "react";
 import { Button, FormGroup, FormControl, FormLabel } from "react-bootstrap";
-import '../styles/Login.css'
-import axios from 'axios';
+import "../styles/Login.css";
+import axios from "axios";
 import { useAuth } from "../context/auth";
 import { Link, Redirect } from "react-router-dom";
-import '../../node_modules/font-awesome/css/font-awesome.min.css';
-
-
+import "../../node_modules/font-awesome/css/font-awesome.min.css";
+import Img from "react-image";
 
 export default function Login(props) {
   const [isLoggedIn, setLoggedIn] = useState(false);
@@ -14,23 +13,27 @@ export default function Login(props) {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const { setAuthTokens } = useAuth;
+  const Img = require("../img/login_image.png");
 
   function postLogin() {
-    axios.post("https://energeapi.do.viewyoursite.net/api/token/", {
-      username,
-      password
-    }).then(result => {
-      if (result.status === 200) {
-        console.log(username);
-        console.log(result.data);
-        setAuthTokens(result.data);
-        setLoggedIn(true);
-      } else {                
+    axios
+      .post("https://energeapi.do.viewyoursite.net/api/token/", {
+        username,
+        password
+      })
+      .then(result => {
+        if (result.status === 200) {
+          console.log(username);
+          console.log(result.data);
+          setAuthTokens(result.data);
+          setLoggedIn(true);
+        } else {
+          setIsError(true);
+        }
+      })
+      .catch(e => {
         setIsError(true);
-      }
-    }).catch(e => {
-      setIsError(true);
-    });
+      });
   }
 
   if (isLoggedIn) {
@@ -48,37 +51,48 @@ export default function Login(props) {
 
   return (
     <div className="Login">
-        <h4 align="center">Login in to</h4>
-        <h4 align="center" id="title">Energe</h4>
-        <p><span className="fas fa-search"></span></p>
-        <center>
-      <form onSubmit={handleSubmit} method="post">
-        <FormGroup controlId="username" bsSize="large" className="padb10">
-          <FormControl
-            autoFocus
-            type="text"
-            placeholder="username"
-            value={username}
-            onChange={e => setUsername(e.target.value)}
-          />
-        </FormGroup>
-        <FormGroup controlId="password" bsSize="large" className="padb10">
-          <FormControl
-            value={password}
-            placeholder="password"
-            onChange={e => setPassword(e.target.value)}
-            type="password"
-          />
-        </FormGroup>
+      <h4 align="center">Login in to</h4>
+      <h4 align="center" id="title">
+        Energe
+      </h4>
+      <p>
+        <span className="fas fa-search"></span>
+      </p>
+      <center>
+        <form onSubmit={handleSubmit} method="post">
+          <FormGroup controlId="username" bsSize="large" className="padb10">
+            <FormControl
+              autoFocus
+              type="text"
+              placeholder="username"
+              value={username}
+              onChange={e => setUsername(e.target.value)}
+            />
+          </FormGroup>
+          <FormGroup controlId="password" bsSize="large" className="padb10">
+            <FormControl
+              value={password}
+              placeholder="password"
+              onChange={e => setPassword(e.target.value)}
+              type="password"
+            />
+          </FormGroup>
 
-        <p className="padb10"><a href="/forgetpass">Forgot your password? Reset!</a></p>
+          <p className="padb10">
+            <a href="/forgetpass">Forgot your password? Reset!</a>
+          </p>
 
-        <Button block bsSize="large" type="submit" className="padb10">
-          Log in
-        </Button>
-      </form>
-      <p><a href="/register">Don't have an account? Sign up!</a></p>
+          <Button block bsSize="large" type="submit" className="padb10">
+            Log in
+          </Button>
+        </form>
+        <p>
+          <a href="/register">Don't have an account? Sign up!</a>
+        </p>
       </center>
+      <div>
+        <img src={Img} style={{ width: "100%" }} />
+      </div>
     </div>
   );
 }
