27086ffeb55a088b801d0bce56c521fac3aef71e
ravi_ccc <ravi.s@maanyait.com>
2019-11-26 20:44:52 +0530

    auth done

--- src/containers/login.js
+++ src/containers/login.js
@@ -6,21 +6,42 @@ import { useAuth } from "../context/auth";
 import { Link, Redirect } from "react-router-dom";
 import '../../node_modules/font-awesome/css/font-awesome.min.css';
 import Img from "react-image";
+import { withRouter } from 'react-router-dom';
+import { PropTypes } from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
 
 
+const image = require("../img/login_image.png");
+const styles = theme => ({});
+// function validateForm() {
+//     return username.length > 0 && password.length > 0;
+// }
+class Login extends React.Component {
+    constructor(props) {
+            super(props);
+            this.state = {
+                isLoggedIn: false,
+                isError: false,
+                username: '',
+                password: '',
+                // setAuthTokens:useAuth
+            }
+        }
+        // const [isLoggedIn, setLoggedIn] = useState(false);
+        // const [isError, setIsError] = useState(false);
+        // const [username, setUsername] = useState("");
+        // const [password, setPassword] = useState("");
+        // const { setAuthTokens } = useAuth;
 
-export default function Login(props) {
-    const [isLoggedIn, setLoggedIn] = useState(false);
-    const [isError, setIsError] = useState(false);
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const { setAuthTokens } = useAuth;
-    const Img = require("../img/login_image.png");
-
-    function postLogin() {
+    //   validateForm=()=> {
+    //     return username.length > 0 && password.length > 0;
+    // }
+    postLogin = (e) => {
+        e.preventDefault();
+        let self = this;
         axios.post("https://energeapi.do.viewyoursite.net/api/token/", {
-            username,
-            password
+            username: self.state.username,
+            password: self.state.password
         }).then(result => {
             if (result.status === 200) {
                 console.log(result.data);
@@ -28,83 +49,102 @@ export default function Login(props) {
                 localStorage.setItem('id_token', result.idToken);
                 //setAuthTokens(result.data);
                 //setLoggedIn(true);
-                return <Redirect to = "/home" / > ;
+                debugger;
+                this.props.history.push({
+                    pathname: "/home"
+                });
             } else {
-                setIsError(true);
+                this.setState({
+                    isError: true
+                });
             }
         }).catch(e => {
-            setIsError(true);
+            this.setState({
+                isError: true
+            });
         });
     }
 
-    if (isLoggedIn) {
-        return <Redirect to = "/home" / > ;
+
+
+    handleUserName = e => {
+        console.log(e.target.value);
+        this.setState({
+            username: e.target.value,
+        });
+    }
+    handlePassword = e => {
+        console.log(e.target.value);
+        this.setState({
+            password: e.target.value,
+        });
     }
 
-    function validateForm() {
-        return username.length > 0 && password.length > 0;
+
+    render() {
+        return ( <
+            div className = "Login" >
+            <
+            h4 align = "center" > Login in to < /h4> <
+            h4 align = "center"
+            id = "title" > Energe < /h4> <
+            p > < span className = "fas fa-search" > < /span></p >
+            <
+            center >
+            <
+            form onSubmit = { this.handleSubmit }
+            method = "post" >
+            <
+            FormGroup controlId = "username"
+            bsSize = "large"
+            className = "padb10" >
+            <
+            FormControl autoFocus type = "text"
+            placeholder = "username"
+            value = { this.state.username }
+            onChange = { this.handleUserName }
+            />  < /
+            FormGroup > <
+            FormGroup controlId = "password"
+            bsSize = "large"
+            className = "padb10" >
+            <
+            FormControl value = { this.state.password }
+            placeholder = "password"
+            onChange = { this.handlePassword }
+            type = "password" /
+            >
+            <
+            /FormGroup>
+
+            <
+            p className = "padb10" > < a href = "/forgetpass" > Forgot your password ? Reset! < /a></p >
+            <
+            Button block bsSize = "large"
+            type = "submit"
+            onClick = { this.postLogin }
+            className = "padb10" >
+            Log in
+            <
+            /Button>  < /
+            form > <
+            p > < a href = "/register" > Don 't have an account? Sign up!</a></p>   < /
+            center > <
+            div >
+            <
+            img src = { image }
+            style = {
+                { width: "100%", height: "fit-content", bottom: 0, position: "absolute" }
+            }
+            />   < /
+            div > <
+            /div >
+        );
     }
 
-    function handleSubmit(event) {
-        event.preventDefault();
-        postLogin();
-    }
-
-    return ( <
-        div className = "Login" >
-        <
-        h4 align = "center" > Login in to < /h4> <
-        h4 align = "center"
-        id = "title" > Energe < /h4> <
-        p > < span className = "fas fa-search" > < /span></p >
-        <
-        center >
-        <
-        form onSubmit = { handleSubmit }
-        method = "post" >
-        <
-        FormGroup controlId = "username"
-        bsSize = "large"
-        className = "padb10" >
-        <
-        FormControl autoFocus type = "text"
-        placeholder = "username"
-        value = { username }
-        onChange = { e => setUsername(e.target.value) }
-        />  < /
-        FormGroup > <
-        FormGroup controlId = "password"
-        bsSize = "large"
-        className = "padb10" >
-        <
-        FormControl value = { password }
-        placeholder = "password"
-        onChange = { e => setPassword(e.target.value) }
-        type = "password" /
-        >
-        <
-        /FormGroup>
-
-        <
-        p className = "padb10" > < a href = "/forgetpass" > Forgot your password ? Reset! < /a></p >
-        <
-        Button block bsSize = "large"
-        type = "submit"
-        className = "padb10" >
-        Log in
-        <
-        /Button>  < /
-        form > <
-        p > < a href = "/register" > Don 't have an account? Sign up!</a></p>   < /
-        center > <
-        div >
-        <
-        img src = { Img }
-        style = {
-            { width: "100%" }
-        }
-        />   < /
-        div > <
-        /div >
-    );
 }
+Login.propTypes = {
+    children: PropTypes.node.isRequired,
+};
+
+export default withRouter(withStyles(styles)(Login));
--- src/containers/register.js
+++ src/containers/register.js
@@ -1,68 +1,158 @@
 import React, { useState } from "react";
 import { Button, FormGroup, FormControl, FormLabel } from "react-bootstrap";
 import '../styles/Login.css'
+import axios from 'axios';
+import { useAuth } from "../context/auth";
+import { Link, Redirect } from "react-router-dom";
+import '../../node_modules/font-awesome/css/font-awesome.min.css';
+import Img from "react-image";
+import { withRouter } from 'react-router-dom';
+import { PropTypes } from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
 
-export default function Register(props) {
-    const [email, setEmail] = useState("");
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirm_password, setcPassword] = useState("");
 
-  function validateForm() {
-    return username.length > 0 && password.length > 0;
-  }
+const image = require("../img/login_image.png");
+const styles = theme => ({});
+// function validateForm() {
+//     return username.length > 0 && password.length > 0;
+// }
+class Register extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLoggedIn: false,
+            isError: false,
+            username: '',
+            email: '',
+            password: '',
+        }
+    }
+    postRegister = (e) => {
+        e.preventDefault();
+        let self = this;
+        axios.post("https://energeapi.do.viewyoursite.net/api/token/", {
+            username: self.state.username,
+            email: self.state.email,
+            password: self.state.password
+        }).then(result => {
+            if (result.status === 200) {
+                console.log(result.data);
+                localStorage.setItem('access_token', result.accessToken);
+                localStorage.setItem('id_token', result.idToken);
+                //setAuthTokens(result.data);
+                //setLoggedIn(true);
+                debugger;
+                this.props.history.push({
+                    pathname: "/home"
+                });
+            } else {
+                this.setState({
+                    isError: true
+                });
+            }
+        }).catch(e => {
+            this.setState({
+                isError: true
+            });
+        });
+    }
 
-  function handleSubmit(event) {
-    event.preventDefault();
-  }
 
-  return (
-    <div className="Register">
-        <h4 align="center">Sign Up for</h4>
-        <h4 align="center" id="title">Energe</h4>
-        <center>
-      <form onSubmit={handleSubmit}>
-        <FormGroup controlId="username" bsSize="large" className="padb10">
-          <FormControl
-            autoFocus
-            type="text"
-            hint="username"
-            placeholder="username"
-            value={username}
-            onChange={e => setUsername(e.target.value)}
-          />
-        </FormGroup>
-        <FormGroup controlId="email" bsSize="large" className="padb10">
-          <FormControl
-            autoFocus
-            type="email"
-            placeholder="Email"
-            value={email}
-            onChange={e => setEmail(e.target.value)}
-          />
-        </FormGroup>
-        <FormGroup controlId="password" bsSize="large" className="padb10">
-          <FormControl
-            placeholder="Password"
-            value={password}
-            onChange={e => setPassword(e.target.value)}
-            type="password"
-          />
-        </FormGroup>
-        <FormGroup controlId="cpassword" bsSize="large" className="padb10">
-          <FormControl
-            placeholder="Confirm Password"
-            value={confirm_password}
-            onChange={e => setcPassword(e.target.value)}
-            type="password"
-          />
-        </FormGroup>
-        <Button block bsSize="large" type="submit" className="padb10">
-          Sign up
-        </Button>
-      </form>
-      <p><a href="/login">Already have an account? Log in!</a></p>
-      </center>
-    </div>
-  );
+
+    handleUserName = e => {
+        console.log(e.target.value);
+        this.setState({
+            username: e.target.value,
+        });
+    }
+    handlePassword = e => {
+        console.log(e.target.value);
+        this.setState({
+            password: e.target.value,
+        });
+    }
+
+
+    render() {
+        return ( <
+            div className = "Register" >
+            <
+            h4 align = "center" > Sign Up
+            for < /h4> <
+            h4 align = "center"
+            id = "title" > Energe < /h4> <
+            center >
+            <
+            form onSubmit = { this.handleSubmit } >
+            <
+            FormGroup controlId = "username"
+            bsSize = "large"
+            className = "padb10" >
+            <
+            FormControl autoFocus type = "text"
+            hint = "username"
+            placeholder = "username"
+            value = { this.state.username }
+            onChange = { this.Username }
+            /> < /
+            FormGroup > <
+            FormGroup controlId = "email"
+            bsSize = "large"
+            className = "padb10" >
+            <
+            FormControl autoFocus type = "email"
+            placeholder = "Email"
+            value = { this.state.email }
+            onChange = { this.Email }
+            /> < /
+            FormGroup > <
+            FormGroup controlId = "password"
+            bsSize = "large"
+            className = "padb10" >
+            <
+            FormControl placeholder = "Password"
+            value = { this.state.password }
+            onChange = { this.Password }
+            type = "password" /
+            >
+            <
+            /FormGroup> <
+            FormGroup controlId = "cpassword"
+            bsSize = "large"
+            className = "padb10" >
+            <
+            FormControl placeholder = "Confirm Password"
+            value = { this.state.confirm_password }
+            onChange = { this.setcPassword }
+            type = "password" /
+            >
+            <
+            /FormGroup> <
+            Button block bsSize = "large"
+            type = "submit"
+            onClick = { this.postRegister }
+            className = "padb10" >
+            Sign up <
+            /Button> < /
+            form > <
+            p > < a href = "/login" > Already have an account ? Log in ! < /a></p >
+            <
+            /center> <
+            div >
+            <
+            img src = { image }
+            style = {
+                { width: "100%", height: "fit-content", bottom: 0, position: "absolute" }
+            }
+            /> < /
+            div > < /
+            div >
+        );
+    }
+
 }
+Register.propTypes = {
+    children: PropTypes.node.isRequired,
+};
+
+export default withRouter(withStyles(styles)(Register));
