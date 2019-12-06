a6674af771c20ce3c2b8a44834ba7129c373d40e
Suman Kumar <suman.k@skysoft.net.in>
2019-12-06 15:24:38 +0530

    =34

--- src/containers/home.js
+++ src/containers/home.js
@@ -10,7 +10,7 @@ class Home extends React.Component {
     render() {
         return (
                 <Grid container direction="row" justify="space-between" alignItems="flex-start" spacing={3}>
-                    <Grid item xs={8}>
+                    <Grid item xs={7}>
                         <Feed />
                     </Grid>
                     <Grid item xs={4}>
