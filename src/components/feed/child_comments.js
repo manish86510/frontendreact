747d347398294206bd88616c00f8d5139a7ce900
sonu <sonu.ka@skysoft.net.in>
2019-12-26 17:07:42 +0530

    +

--- src/components/feed/child_comments.js
+++ src/components/feed/child_comments.js
@@ -47,11 +47,11 @@ class ChildComments extends React.Component {
     state = {
         childComments: [],
     };
-    // componentDidMount() {
-    //     this.setState({
-    //         childComments: this.props.childComments
-    //     });
-    // }
+    componentDidMount() {
+        this.setState({
+            childComments: this.props.childComments
+        });
+    }
     replyComment = (comment_id) => {
         this.setState({
             comment_on_post: comment_id,
@@ -73,27 +73,25 @@ class ChildComments extends React.Component {
         });
         this.props.onCommented();
     }
-    // handleExpandMoreClick = (id) => {
-    //     console.log("id :", id);
-    //     var childCommentexpandMore = {
-    //         id: id,
-    //         status: true
-    //     }
-    //     this.setState({
-    //         openNestedChildList: childCommentexpandMore
-    //     });
-    // }
+    handleExpandMoreClick = (id) => {
+        var childCommentexpandMore = {
+            id: id,
+            status: true
+        }
+        this.setState({
+            openNestedChildList: childCommentexpandMore
+        });
+    }
 
-    // handleExpandLessClick = (id) => {
-    //     console.log("id :", id);
-    //     var childCommentexpandless = {
-    //         id: id,
-    //         status: false
-    //     }
-    //     this.setState({
-    //         openNestedChildList: childCommentexpandless
-    //     });
-    // }
+    handleExpandLessClick = (id) => {
+        var childCommentexpandless = {
+            id: id,
+            status: false
+        }
+        this.setState({
+            openNestedChildList: childCommentexpandless
+        });
+    }
 
     render() {
         console.log(this.state.childComments);
@@ -121,7 +119,7 @@ class ChildComments extends React.Component {
                                         <ListItemSecondaryAction>
                                             <Button color="primary" onClick={this.replyComment.bind(this, child_comment.id)}> Reply</Button>
                                             
-                                            {/* {
+                                            {
                                                 (child_comment.children.length > 0) ?
                                                    this.state.openNestedChildList.id === child_comment.id 
                                                    && 
@@ -135,24 +133,24 @@ class ChildComments extends React.Component {
                                                     <IconButton disabled size="small" onClick={this.handleExpandLessClick.bind(this, child_comment.id)}>
                                                         <ExpandMore />
                                                     </IconButton>
-                                            } */}
+                                            }
                                         </ListItemSecondaryAction>
                                     ) : undefined
                                 }
                             </ListItem>
                             <List disablePadding>
-                                {/* <Collapse
+                                <Collapse
                                     in={
                                         this.state.openNestedChildList.id === child_comment.id ?
                                             this.state.openNestedChildList.status : false
                                     }
                                     timeout="auto"
                                     unmountOnExit
-                                > */}
+                                >
                                     <ListItem style={{ paddingLeft: "8%" }} alignItems="flex-start">
-                                        <ChildComments post={this.state.post} onCommented={this.onChildCommented} childComments={child_comment.children} />
+                                    <ChildComments post={this.state.post} onCommented={this.onChildCommented} childComments={child_comment.children} />
                                     </ListItem>
-                                {/* </Collapse> */}
+                                </Collapse>
 
                             </List>
                         </div>
